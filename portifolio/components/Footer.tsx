7e75813dab42c94d2d521aa6d69e94e904332068
Patrick Nambajimana <patricknambajimana6@gmail.com>
2025-11-09 17:48:11 +0200

    initial commit

---
+++ portifolio/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white text-center p-4">
+      © {new Date().getFullYear()} Patrick Nambajimana. All rights reserved.
+    </footer>
+  );
+}
---
+++ portifolio/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-900 text-white p-4">
+      <h1 className="text-lg font-bold">Patrick’s Portfolio</h1>
+    </nav>
+  );
+}
