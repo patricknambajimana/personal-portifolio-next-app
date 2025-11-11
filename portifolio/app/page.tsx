6874d0ad04aab1be33da3b552815107ddf26a17f
Patrick Nambajimana <patricknambajimana6@gmail.com>
2025-11-11 22:41:39 +0200

    address added

--- portifolio/app/page.tsx
+++ portifolio/app/page.tsx
@@ -2,8 +2,39 @@
 
 import SkillsCard from "@/components/SkillsCard";
 import { TypeAnimation } from "react-type-animation";
-
+import { Mail } from "lucide-react";
+import { Github } from "lucide-react";
+import { Instagram } from "lucide-react";
+import { Linkedin } from "lucide-react";
 const page = () => {
+  const items = [
+    {
+      icon: <Mail />,
+      title: "email",
+      text: "patricknambajimana6@gmail.com",
+    },
+    {
+      icon: <Github />,
+      title: "github",
+      link: "https://github.com/patricknambajimana",
+      text: "github.com/patricknambajimana",
+    },
+    {
+      icon: <Linkedin />,
+      title: "linkedin",
+      text: "linkedin/patricknambaijimana",
+    },
+    {
+      icon: <Instagram />,
+      title: "instagram",
+      text: "Mr Patrick",
+    },
+    {
+      title: "whatsapp",
+      text: "tel:0781138331",
+    },
+  ];
+
   return (
     <section className="text-center py-2 ">
       <div className="place-items-center">
@@ -16,7 +47,7 @@ const page = () => {
       <div className="mt-4 text-cyan-600 text-4xl font-bold  h-10 place-items-center-safe">
         <TypeAnimation
           sequence={[
-            "I’m Patrick Nambajimana —a passionate software developer.",
+            "I’m Patrick Nambajimana —software developer|AI solution.",
             1000,
             "",
             1000,
@@ -27,40 +58,44 @@ const page = () => {
           cursor={false}
         />
       </div>
+      <div className="flex items-center justify-center w-full ">
+        <p className="text-2xl text-gray-500 text-center max-w-2xl">
+          Specializing in React + TypeScript, Node.js, Python, Machine Learning,
+          and Scalable Web Applications
+        </p>
+      </div>
       <div className="mt-10 flex gap-10 justify-center text-center capitalize ">
         <button className="p-4 bg-cyan-700 text-cyan-200 rounded-2xl capitalize ">
-          view profile
+          view my work
         </button>
 
         <button className="p-4 border border-cyan-300 rounded-2xl text-cyan-200 capitalize">
-          download cv
+          getin touch
         </button>
       </div>
-      <div className="flex justify-between p-10 m t-10 text-cyan-300  animation ">
-        <div className="text-left">
-
-          <span className="capitalize">email</span>
-          <p>patricknambajimana6@gmail.com</p>
-        </div>
-        <div className="text-left">
-
-          <span className="capitalize">github</span><br></br>
-          <a href="https://github.com/patricknambajimana">github.com/patricknambajimana</a>
-        </div>
-        <div className="text-left">
-
-          <span className="capitalize">linkedin</span>
-          <p>linkedin/patricknambaijimana</p>
-        </div>
-        <div className="text-left">
-
-          <span className="capitalize">instagram</span>
-          <p>Mr Patrick</p>
-        </div>
-        <div className="text-left">
-
-          <span className="capitalize">whatsapp</span>
-          <p>tel:0781138331</p>
+      <div className="overflow-hidden">
+        <div className="flex animate-scroll space-x-20 p-10 mt-10 text-cyan-300 w-max">
+          {[...Array(2)].map((_, i) => (
+            <div key={i} className="flex space-x-20">
+              {items.map((item, index) => (
+                <div key={index} className="text-left min-w-[200px]">
+                  {item.icon && <div className="mb-2">{item.icon}</div>}
+                  <p className="capitalize font-semibold">{item.title}</p>
+                  {item.link ? (
+                    <a
+                      href={item.link}
+                      target="_blank"
+                      className="hover:underline"
+                    >
+                      {item.text}
+                    </a>
+                  ) : (
+                    <p>{item.text}</p>
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
         </div>
       </div>
       <div>
--- portifolio/components/SkillsCard.tsx
+++ portifolio/components/SkillsCard.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-type Props = {
-  id: number;
-  name: string;
-};
-
-const SkillsCard = (props: Props) => {
+const SkillsCard = () => {
   const project = [{ name: "Aguka system" }, { name: "RwaRise system" }];
 
   return (
     <div>
       <h2 className="text-3xl text-cyan-300">proffesional experiance</h2>
-      <ul className="flex   text-3xl text-cyan-300"><li><span className="flex gap-10">{project.map((skill) => skill.name)}</span></li></ul>
+      <ul className="flex text-3xl text-cyan-300">
+        {project.map((skill, idx) => (
+          <li key={idx} className="mr-6">
+            <div className="inline">{skill.name}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
